9a527da853bae1deea742ae7cbfc42f5636f6a42
Addison Crump <addisoncrump@tamu.edu>
2018-03-20 18:14:09 -0500

    Make registry modifiable

--- src/main/java/io/github/trulyfree/discordbot/mcreg/Config.java
+++ src/main/java/io/github/trulyfree/discordbot/mcreg/Config.java
@@ -3,24 +3,19 @@ package io.github.trulyfree.discordbot.mcreg;
 import co.templex.oauth.SuccessfulOAuthResponse;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.experimental.FieldDefaults;
 
-import java.util.Collections;
-import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 @FieldDefaults(makeFinal = true,
                level = AccessLevel.PRIVATE)
 @AllArgsConstructor
 public class Config {
-    Registry registry;
+    @Getter(AccessLevel.PACKAGE) Registry registry;
 
     public Config() {
         registry = new Registry();
-    }
-
-    public Map<String, SuccessfulOAuthResponse> getRegistry() {
-        return Collections.unmodifiableMap(registry);
     }
 
     public static class Registry extends ConcurrentHashMap<String, SuccessfulOAuthResponse> {
